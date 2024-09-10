dfa2540d637f6abb3c49af85153bc108c43073bc
Scott Vaughan <Scott.Vaughan@onthebeach.co.uk>
2024-09-10 14:11:34 +0100

    Add project files.

---
+++ Pizza.Api/Controllers/PizzaController.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Pizza.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+        
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Models.Pizza pizza)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(Guid id, Models.Pizza pizza)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Pizza.Api/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace Pizza.Api.Models;
+
+public class Pizza
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Size { get; set; }
+    public DateTime DateCreated { get; }
+}
